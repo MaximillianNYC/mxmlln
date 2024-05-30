20e3b6312544e6275c62793a534641d36ca3a5f6
MaximillianNYC <max@mxmlln.com>
2024-05-30 11:01:29 -0400

    adjust express app routing

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const { generateMeta } = require('./controllers/openaiController')
 
 const app = express()
 app.use(cors());
-app.listen(8080, () => console.log('listening for requests on port 8080'))
-
 app.use(express.json())
-app.use(express.static('public'))  // Make sure your HTML file is in the 'public' directory
+app.use(express.static('public'));
+//app.listen(8080, () => console.log('listening for requests on port 8080'))
 
 app.post('/openai/meta', generateMeta)
 
 // Serve HTML file
 app.get('/', (req, res) => {
-    res.sendFile('path_to_your_html_file.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
 });
+
+//app.use(express.json())
+//app.use(express.static('public'))  // Make sure your HTML file is in the 'public' directory
+
