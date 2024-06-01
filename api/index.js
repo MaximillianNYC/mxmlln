0817775dcea89734e4e6debece226be495531168
MaximillianNYC <max@mxmlln.com>
2024-05-31 23:04:56 -0400

    adding api route with vercel config

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors');
+//const { createProxyMiddleware } = require('http-proxy-middleware')
+const { generateMeta } = require('././controllers/openaiController')
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+app.use(express.static('public'));
+//app.listen(8080, () => console.log('listening for requests on port 8080'))
+
+app.post('/openai/meta', generateMeta)
+
+// Serve HTML file
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
+
+//app.use(express.json())
+//app.use(express.static('public'))  // Make sure your HTML file is in the 'public' directory
