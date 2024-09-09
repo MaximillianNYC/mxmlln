bdffb730b377574aec0d1e41d5383102413ee526
MaximillianNYC <max@mxmlln.com>
2024-09-09 11:01:32 -0500

    NavBar InfoView style complete

--- public/components/NavBar.js
+++ public/components/NavBar.js
@@ -1,3 +1,72 @@
+//INFO VIEW
+    //OPEN INFO VIEW
+    function openNavInfoView() {    
+        document.body.style.overflow = 'hidden';
+
+        //HIDE CONTROLS
+        document.getElementById('navBarSegController').style.transition = 'opacity 0.25s';
+        document.getElementById('navBarSegController').style.opacity = 0;
+        document.getElementById('navBarSegController').style.visibility = 'hidden';
+        document.getElementById('navBar').style.bottom = '-40px';
+
+        //DISPLAY OVERLAY
+        document.getElementById('navBarOverlay').style.transition = 'opacity 0.5s';
+        document.getElementById('navBarOverlay').style.visibility = 'visible';
+        document.getElementById('navBarOverlay').style.opacity = 1;
+
+        //EXPAND CONTAINER
+        document.getElementById('navDynamicContainer').style.transition = 'width 0.5s, height 0.5s, left 0.15s';
+        document.getElementById('navDynamicContainer').style.height = '450px';
+
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            setTimeout(function() {
+                document.getElementById('navDynamicContainer').style.borderRadius = '0px';
+                document.getElementById('navDynamicContainer').style.height = '100%';
+                document.getElementById('navDynamicContainer').style.maxHeight = '100dvh';
+                document.getElementById('navDynamicContainer').style.width = '100vw';
+                document.getElementById('navDynamicContainer').style.maxWidth = '100vw';
+                document.getElementById('navDynamicContainer').style.left = '-4px';
+                document.getElementById('navDynamicContainer').style.bottom = '-2px';
+            }, 200);
+        } else {
+        }
+        document.getElementById('infoViewContent').style.transition = 'visibility 1s, opacity 1.5s';
+        document.getElementById('infoViewContent').style.visibility = 'visible';
+        setTimeout(() => {
+            document.getElementById('infoViewContent').style.opacity = '1';
+        }, 50);
+    }
+
+    //CLOSE INFO VIEW
+    function closeNavInfoView() {
+        document.body.style.overflow = 'scroll';
+        
+        document.getElementById('infoViewContent').style.transition = 'visibility 0.15s, opacity 0.15s';
+        document.getElementById('infoViewContent').style.visibility = 'hidden';
+        document.getElementById('infoViewContent').style.opacity = 0;
+        document.getElementById('infoViewContent').style.zIndex = '-1';
+
+        document.getElementById('navDynamicContainer').style.transition = 'left 0.2s, bottom 0.2s, width 0.15s, height 0.15s';
+        document.getElementById('navDynamicContainer').style.left = 'calc(50% - 232px)'; 
+        document.getElementById('navDynamicContainer').style.bottom = '42px';   
+        document.getElementById('navDynamicContainer').style.width = '456px';
+        document.getElementById('navDynamicContainer').style.height = '64px';
+        document.getElementById('navDynamicContainer').style.borderRadius = '40px';
+        document.getElementById('navDynamicContainer').style.background = 'rgba(240, 240, 240, 0.75)';
+
+        document.getElementById('navBarOverlay').style.transition = 'opacity 1s';
+        document.getElementById('navBarOverlay').style.opacity = 0;
+        setTimeout(function() {
+            document.getElementById('navBarOverlay').style.visibility = 'hidden';
+        }, 1000);
+
+        document.getElementById('navBar').style.bottom = '0px';
+        document.getElementById('navBarSegController').style.visibility = 'visible';
+        document.getElementById('navBarSegController').style.transition = 'opacity 1.5s';
+        document.getElementById('navBarSegController').style.opacity = 1;
+    }
+
+
 //CHAT VIEW
     //OPEN CHAT VIEW
     function openNavChatView() {    
@@ -63,6 +132,7 @@
         }
     }
 
+    //CLOSE CHAT VIEW
     function closeNavChatView() {
         document.body.style.overflow = 'scroll';
         
@@ -115,21 +185,6 @@
             document.getElementById('navDynamicContainer').style.height = '220px';
 
             if (window.matchMedia("(max-width: 768px)").matches) {
-                document.getElementById('chatInputField').addEventListener('focus', function() {
-                    setTimeout(function() {
-                        document.getElementById('navDynamicContainer').style.transition = 'height 0.5s, bottom 0.5s';
-                        document.getElementById('navDynamicContainer').style.height = '50vh';
-                        document.getElementById('navDynamicContainer').style.bottom = '-2px';
-                        document.body.style.overflow = 'hidden';
-                    }, 500);
-                });
-                document.getElementById('chatInputField').addEventListener('blur', function() {
-                    setTimeout(function() {
-                        document.getElementById('navDynamicContainer').style.transition = 'height 0.5s, bottom 0.5s';
-                        document.getElementById('navDynamicContainer').style.height = '100%';
-                        document.getElementById('navDynamicContainer').style.bottom = '-2px';
-                    }, 500);
-                });
                 setTimeout(function() {
                     document.getElementById('navDynamicContainer').style.borderRadius = '0px';
                     document.getElementById('navDynamicContainer').style.height = '100%';
