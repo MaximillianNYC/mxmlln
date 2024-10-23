03551ef3b94a91f43af49213914b6f66fe30839f
MaximillianNYC <max@mxmlln.com>
2024-10-23 19:36:06 -0400

    image flip text

--- public/components/FolioEngine.js
+++ public/components/FolioEngine.js
@@ -401,7 +401,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
         });
-    }, 1500); 
+    }, 1000); 
 });
 
 function collapseExpandedStack() {
