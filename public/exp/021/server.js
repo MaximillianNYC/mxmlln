e596a023301f9c3ef1c160702f25ce66e0f7bb75
MaximillianNYC <max@mxmlln.com>
2025-10-24 22:20:04 -0700

    change routes to avoid overlap

--- public/exp/021/server.js
+++ public/exp/021/server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3001; // Different port to avoid conflicts
+const PORT = 3002; // Different port to avoid conflicts
 
 // Middleware
 app.use(cors());
