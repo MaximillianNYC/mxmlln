129967a3de2a4c76a6b2eb208db27a9439899a29
MaximillianNYC <max@mxmlln.com>
2025-11-26 21:59:18 -0800

    text animation

--- public/exp/029/.next/server/chunks/ssr/public_exp_029_f7bebf88._.js
+++ public/exp/029/.next/server/chunks/ssr/public_exp_029_f7bebf88._.js
@@ -11,74 +11,100 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node
 ;
 function Page() {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-        className: "h-screen w-screen min-h-screen min-w-full overflow-hidden m-0 p-0",
-        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("svg", {
-            className: "fixed top-0 left-0 w-screen h-screen",
-            style: {
-                mask: "url(#circleMask)",
-                WebkitMask: "url(#circleMask)"
-            },
-            width: "100%",
-            height: "100%",
-            preserveAspectRatio: "xMidYMid slice",
-            children: [
-                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("defs", {
-                    children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("mask", {
-                        id: "circleMask",
-                        children: [
-                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("rect", {
-                                width: "100%",
-                                height: "100%",
-                                fill: "black"
-                            }, void 0, false, {
-                                fileName: "[project]/public/exp/029/src/app/page.tsx",
-                                lineNumber: 18,
-                                columnNumber: 13
-                            }, this),
-                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("circle", {
-                                id: "maskCircle",
-                                cx: "50%",
-                                cy: "50%",
-                                fill: "white",
-                                className: "origin-center animate-shrink-circle blur-[10px]",
-                                style: {
-                                    transformOrigin: "center"
-                                }
-                            }, void 0, false, {
-                                fileName: "[project]/public/exp/029/src/app/page.tsx",
-                                lineNumber: 19,
-                                columnNumber: 13
-                            }, this)
-                        ]
-                    }, void 0, true, {
+        className: "h-screen w-screen min-h-screen min-w-full overflow-hidden m-0 p-0 relative",
+        children: [
+            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("svg", {
+                className: "fixed top-0 left-0 w-screen h-screen",
+                style: {
+                    mask: "url(#circleMask)",
+                    WebkitMask: "url(#circleMask)"
+                },
+                width: "100%",
+                height: "100%",
+                preserveAspectRatio: "xMidYMid slice",
+                children: [
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("defs", {
+                        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("mask", {
+                            id: "circleMask",
+                            children: [
+                                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("rect", {
+                                    width: "100%",
+                                    height: "100%",
+                                    fill: "black"
+                                }, void 0, false, {
+                                    fileName: "[project]/public/exp/029/src/app/page.tsx",
+                                    lineNumber: 18,
+                                    columnNumber: 13
+                                }, this),
+                                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("circle", {
+                                    id: "maskCircle",
+                                    cx: "50%",
+                                    cy: "42%",
+                                    fill: "white",
+                                    className: "origin-center animate-shrink-circle blur-[10px]",
+                                    style: {
+                                        transformOrigin: "center"
+                                    }
+                                }, void 0, false, {
+                                    fileName: "[project]/public/exp/029/src/app/page.tsx",
+                                    lineNumber: 19,
+                                    columnNumber: 13
+                                }, this)
+                            ]
+                        }, void 0, true, {
+                            fileName: "[project]/public/exp/029/src/app/page.tsx",
+                            lineNumber: 17,
+                            columnNumber: 11
+                        }, this)
+                    }, void 0, false, {
                         fileName: "[project]/public/exp/029/src/app/page.tsx",
-                        lineNumber: 17,
-                        columnNumber: 11
+                        lineNumber: 16,
+                        columnNumber: 9
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("image", {
+                        href: "/bg.png",
+                        x: "0",
+                        y: "0",
+                        width: "100%",
+                        height: "100%",
+                        preserveAspectRatio: "xMidYMid slice"
+                    }, void 0, false, {
+                        fileName: "[project]/public/exp/029/src/app/page.tsx",
+                        lineNumber: 29,
+                        columnNumber: 9
                     }, this)
-                }, void 0, false, {
-                    fileName: "[project]/public/exp/029/src/app/page.tsx",
-                    lineNumber: 16,
-                    columnNumber: 9
-                }, this),
-                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("image", {
-                    href: "/bg.png",
-                    x: "0",
-                    y: "0",
-                    width: "100%",
-                    height: "100%",
-                    preserveAspectRatio: "xMidYMid slice"
-                }, void 0, false, {
-                    fileName: "[project]/public/exp/029/src/app/page.tsx",
-                    lineNumber: 29,
-                    columnNumber: 9
-                }, this)
-            ]
-        }, void 0, true, {
-            fileName: "[project]/public/exp/029/src/app/page.tsx",
-            lineNumber: 6,
-            columnNumber: 7
-        }, this)
-    }, void 0, false, {
+                ]
+            }, void 0, true, {
+                fileName: "[project]/public/exp/029/src/app/page.tsx",
+                lineNumber: 6,
+                columnNumber: 7
+            }, this),
+            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+                className: "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center",
+                children: [
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+                        className: "h-[100px] mb-6"
+                    }, void 0, false, {
+                        fileName: "[project]/public/exp/029/src/app/page.tsx",
+                        lineNumber: 39,
+                        columnNumber: 9
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$public$2f$exp$2f$029$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
+                        className: "text-black text-4xl font-bold animate-fade-in-up whitespace-nowrap",
+                        children: "hello world"
+                    }, void 0, false, {
+                        fileName: "[project]/public/exp/029/src/app/page.tsx",
+                        lineNumber: 40,
+                        columnNumber: 9
+                    }, this)
+                ]
+            }, void 0, true, {
+                fileName: "[project]/public/exp/029/src/app/page.tsx",
+                lineNumber: 38,
+                columnNumber: 7
+            }, this)
+        ]
+    }, void 0, true, {
         fileName: "[project]/public/exp/029/src/app/page.tsx",
         lineNumber: 5,
         columnNumber: 5
--- public/exp/029/src/app/page.tsx
+++ public/exp/029/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Page() {
   return (
-    <div className="h-screen w-screen min-h-screen min-w-full overflow-hidden m-0 p-0">
+    <div className="h-screen w-screen min-h-screen min-w-full overflow-hidden m-0 p-0 relative">
       <svg
         className="fixed top-0 left-0 w-screen h-screen"
         style={{
@@ -19,7 +19,7 @@ export default function Page() {
             <circle
               id="maskCircle"
               cx="50%"
-              cy="50%"
+              cy="42%"
               fill="white"
               className="origin-center animate-shrink-circle blur-[10px]"
               style={{ transformOrigin: "center" }}
@@ -35,6 +35,12 @@ export default function Page() {
           preserveAspectRatio="xMidYMid slice"
         />
       </svg>
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+        <div className="h-[100px] mb-6"></div>
+        <h1 className="text-black text-4xl font-bold animate-fade-in-up whitespace-nowrap">
+          hello world
+        </h1>
+      </div>
     </div>
   );
 }
